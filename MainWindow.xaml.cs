70f909481027e53ea3f9b4110cf9a99fe8b0b53d
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-11-15 05:29:16 +0100

    Deleted own implemenation of database connection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,29 +22,7 @@ namespace WpfZooManager
         {
             InitializeComponent();
 
-            // Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Daten\sourcecode\csharp\WpfToDoApp\WpfToDoDB.accdb
-            string connectionString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)}; Dbq=C:\\Daten\\sourcecode\\csharp\\WpfToDoApp\\WpfToDoDB.accdb; Uid=Admin; Pwd=;";
 
-            using (OdbcConnection connection = new OdbcConnection(connectionString))
-            {
-
-                string query = "SELECT id, location FROM Zoo"; 
-                OdbcCommand command = new OdbcCommand(query, connection);
-                connection.Open();
-                
-                OdbcDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    TextBlock todoItem = new TextBlock
-                    {
-                        Text = reader.GetString(1),
-                        Margin = new Thickness(10)
-                    };
-                    ZooList.Children.Add(todoItem);
-
-                }
-
-            }
         }
     }
 }
