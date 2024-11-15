29687966714233f077f4d84b04e189e298884680
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-11-15 05:55:50 +0100

    Read zoo data from database and display it in list box

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace WpfZooManager
 {
     /// <summary>
@@ -18,10 +19,32 @@ namespace WpfZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        OdbcConnection odbcConnection;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            string connectionString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)}; Dbq=C:\\Daten\\sourcecode\\csharp\\WpfZooManager\\WpfZooManagerDB.accdb; Uid=Admin; Pwd=;";
+            odbcConnection = new OdbcConnection(connectionString);
+            ShowZoos();
+
+        }
+
+        private void ShowZoos()
+        {
+            string query = "SELECT * FROM Zoo";
+            OdbcDataAdapter odbcDataAdapter = new OdbcDataAdapter(query, odbcConnection);
+
+            using (odbcDataAdapter)
+            { 
+                DataTable zooTable = new DataTable();
+                odbcDataAdapter.Fill(zooTable);
+
+                listZoos.DisplayMemberPath = "Location";
+                listZoos.SelectedValuePath = "Id";
+                listZoos.ItemsSource = zooTable.DefaultView;
+            }
 
         }
     }
