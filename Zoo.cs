d6044ae31d7174cfba51812a479fcd45e125ce36
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-11-21 06:26:02 +0100

    Added classes of Zoo and Animal which I forgot to add to the previous commits

---
+++ Zoo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfZooManager
+{
+
+    public class Zoo
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+    }
+}
