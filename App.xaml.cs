d6dc06664af7f8ae69f2fc6a980d478a7aecf944
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-11-26 19:47:55 +0100

    Added LoginWindow to add credentials

--- App.xaml.cs
+++ App.xaml.cs
@@ -18,15 +18,15 @@ namespace WpfZooManager
 
             collection.AddSingleton<MainWindow>();
 
-            string server = Environment.GetEnvironmentVariable("DB_SERVER");
-            string database = Environment.GetEnvironmentVariable("DB_DATABASE");
-            string userId = Environment.GetEnvironmentVariable("DB_USER");
-            string password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            //string server = Environment.GetEnvironmentVariable("DB_SERVER");
+            //string database = Environment.GetEnvironmentVariable("DB_DATABASE");
+            //string userId = Environment.GetEnvironmentVariable("DB_USER");
+            //string password = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
-            string connectionString = $"Server=tcp:{server},1433;Initial Catalog={database};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;User ID={userId};Password={password}";
-            IDbConnection db = new SqlConnection(connectionString);
-            collection.AddSingleton(db);
-            collection.AddSingleton<IZooManagerRepository, ZooManagerRepository>();
+            //string connectionString = $"Server=tcp:{server},1433;Initial Catalog={database};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;User ID={userId};Password={password}";
+            //IDbConnection db = new SqlConnection(connectionString);
+            //collection.AddSingleton(db);
+            //collection.AddSingleton<IZooManagerRepository, ZooManagerRepository>();
 
             var serviceProvider = collection.BuildServiceProvider();
             var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
